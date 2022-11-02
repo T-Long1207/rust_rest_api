d740e2ab01c9f29cf3e4d877ff22e682ea2e9a7f
MrHealer <Longclan1@>
2022-11-03 00:17:45 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,9 @@ use listenfd::ListenFd;
 use std::env;
 
 mod db;
+mod employees;
 mod error_handler;
-mod phones;
 mod schema;
-
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -20,7 +19,7 @@ async fn main() -> std::io::Result<()> {
     db::init();
 
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().configure(phones::init_routes));
+    let mut server = HttpServer::new(|| App::new().configure(employees::init_routes));
 
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
--- src/schema.rs
+++ src/schema.rs
@@ -1,100 +1,10 @@
 table! {
-    phone_report (id) {
+    employees (id) {
         id -> Int4,
-        phone_number -> Varchar,
-        origin_name -> Text,
-        count_confirm -> Int4,
-        count_report -> Int4,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    phone_search (id) {
-        id -> Int4,
-        phone_number -> Varchar,
-        count_search -> Int4,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    phone_comment (id) {
-        id -> Int4,
-        phone_id -> Int4,
-        name_user -> Varchar,
-        body_comment -> Text,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-
-table! {
-    app_report (id) {
-        id -> Int4,
-        app_name -> Varchar,
-        app_image -> Varchar,
-        app_bundle_android -> Varchar,
-        app_bundle_ios -> Varchar,
-        app_des -> Text,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    app_comment (id) {
-        id -> Int4,
-        app_id -> Int4,
-        name_user -> Varchar,
-        body_comment -> Text,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    link_report (id) {
-        id -> Int4,
-        link_des -> Varchar,
-        origin_name -> Text,
-        count_confirm -> Int4,
-        count_report -> Int4,
-        category_id - Int4,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    link_category (id) {
-        id -> Int4,
-        type_link -> Varchar,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    link_search (id) {
-        id -> Int4,
-        link_des -> Varchar,
-        count_search -> Int4,
-        date_created -> Varchar,
-        date_modified -> Varchar,
-    }
-}
-
-table! {
-    link_comment (id) {
-        id -> Int4,
-        link_id -> Int4,
-        name_user -> Varchar,
-        body_comment -> Text,
-        date_created -> Varchar,
-        date_modified -> Varchar,
+        first_name -> Varchar,
+        last_name -> Varchar,
+        department -> Varchar,
+        salary -> Int4,
+        age -> Int4,
     }
 }
